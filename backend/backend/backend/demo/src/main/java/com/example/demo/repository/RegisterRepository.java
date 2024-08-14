bdaf1ac8017566ddaf0a214bb183090ba046edc9
LeviAcckerman <153718142+LeviAcckerman@users.noreply.github.com>
2024-08-14 10:15:10 +0530

    Delete backend directory

--- backend/backend/backend/demo/src/main/java/com/example/demo/repository/RegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Register;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegisterRepository extends CrudRepository<Register, Long> {
-}
